88953fdc7463e26c6bc1b0ba7d96f91ae5721a1b
eden.lee <eden.in.void@gmail.com>
2025-06-02 21:15:32 +0900

    refactor: MinesweeperGame (ISP)

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,14 +1,15 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
-import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
 
 public class GameApplication {
 
     public static void main(String[] args) {
-        GameLevel gameLevel = new VeryBeginner();
+        GameLevel gameLevel = new Beginner();
 
         Minesweeper minesweeper = new Minesweeper(gameLevel);
+        minesweeper.initialize();
         minesweeper.run();
     }
 
