55f05e92660e8e771146520255caefae97dd95de
eden.lee <eden.in.void@gmail.com>
2025-06-02 15:47:37 +0900

    refactor: MinesweeperGame (OCP)

--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
@@ -3,6 +3,9 @@ package cleancode.minesweeper.tobe.io;
 import cleancode.minesweeper.tobe.GameBoard;
 import cleancode.minesweeper.tobe.GameException;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class ConsoleOutputHandler {
 
     public void showGameStartComments() {
@@ -12,9 +15,11 @@ public class ConsoleOutputHandler {
     }
 
     public void showBoard(GameBoard board) {
-        System.out.println("   a b c d e f g h i j");
+        String alphabets = generateColAlphabets(board);
+
+        System.out.println("    " + alphabets);
         for (int row = 0; row < board.getRowSize(); row++) {
-            System.out.printf("%d  ", row + 1);
+            System.out.printf("%2d  ", row + 1);
             for (int col = 0; col < board.getColSize(); col++) {
                 System.out.print(board.getSign(row, col) + " ");
             }
@@ -23,11 +28,19 @@ public class ConsoleOutputHandler {
         System.out.println();
     }
 
+    private String generateColAlphabets(GameBoard board) {
+        List<String> alphabets = IntStream.range(0, board.getColSize())
+            .mapToObj(index -> (char) ('a' + index))
+            .map(Object::toString)
+            .toList();
+        return String.join(" ", alphabets);
+    }
+
     public void printGameWinningComment() {
         System.out.println("지뢰를 모두 찾았습니다. GAME CLEAR!");
     }
 
-    public void pringGameLosingComment() {
+    public void printGameLosingComment() {
         System.out.println("지뢰를 밟았습니다. GAME OVER!");
     }
 
